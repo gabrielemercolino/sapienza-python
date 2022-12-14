07ff6c2c277a41a6ed16e7a4bca7a6825cdb516f
gabrielemercolino <gmercolino2003@gmail.com>
2022-12-14 21:17:51 +0100

    cancellato tutto che non va una ceppa

--- homework/8 - obbligatorio/program01.py
+++ homework/8 - obbligatorio/program01.py
@@ -56,100 +56,30 @@ funzione usata per la soluzione deve essere ricorsiva.
 '''
 
 
-def fill_table(table: list[list], black_plays: bool):
-    empty_pos = []
-    for r, row in enumerate(table):
-        for c, col in enumerate(row):
-            if table[r][c] == ".":
-                empty_pos.append([r, c])
-
-    print(*table, sep="\n", end="\n\n")
-    if not empty_pos:
-        return
-
-    neighbors = [(0, 1),  # E
-                 (-1, 1),  # NE
-                 (-1, 0),  # N
-                 (-1, -1),  # NW
-                 (0, -1),  # W
-                 (1, -1),  # SW
-                 (1, 0),  # S
-                 (1, 1),  # SE
-                 ]
-
-    r, c = empty_pos[0][0], empty_pos[0][1]
-    table[r][c] = "B" if black_plays else "W"
-    change_neighbour_recursive(table, neighbors, len(table), len(table[0]), r, c, black_plays)
-    return fill_table(table, not black_plays)
-
-
-def change_neighbour_recursive(table, neighbours, H, W, r, c, black_plays):
-    if not neighbours:
-        return
-    first = neighbours[0]
-    to_check_r, to_check_c = (r + first[0]) % H, (c + first[1]) % W
-    to_check = table[to_check_r][to_check_c]
-    if to_check != ".":
-        if to_check == "W" and black_plays:
-            table[to_check_r][to_check_c] = "B"
-        elif to_check == "B" and not black_plays:
-            table[to_check_r][to_check_c] = "W"
-
-    return change_neighbour_recursive(table, neighbours[1:], H, W, r, c, black_plays)
+def get_all_combinations_generator(lis):
+    if len(lis) == 1:
+        yield lis
+    else:
+        for i in range(len(lis)):
+            combs = get_all_combinations_generator(lis[:i] + lis[i + 1:])
+            for c in combs:
+                yield [lis[i], *c]
 
 
 def dumbothello(filename: str) -> tuple[int, int, int]:
     table = []
 
     with open(filename) as f:
-        for line in f.readlines():
-            table.append(line.split())
-
-    # fill_table(table, black_plays=True)
+        table.extend(line.split() for line in f)
 
     endings = [0, 0, 0]
 
-    play(table, endings=endings)
-    # print(table)
     return tuple(endings)
-
-
-def play(start_table, endings, free_pos=None, table=None, black_turn=True):
-    if not table:
-        table = [[col for col in row] for row in start_table]
-    elif not any("." in row for row in table):
-        num_W = sum([col.count("W") for row in table for col in row])
-        num_B = sum([col.count("B") for row in table for col in row])
-        if num_B > num_W:
-            endings[0] += 1
-        elif num_B < num_W:
-            endings[1] += 1
-        else:
-            endings[2] += 1
-        print(*table, sep="\n", end="\n\n")
-        return table
-    if not free_pos:
-        free_pos = []
-        for r, row in enumerate(table):
-            for c, col in enumerate(row):
-                if col == ".":
-                    free_pos.append([r, c])
-    neighbors = [(0, 1),  # E
-                 (-1, 1),  # NE
-                 (-1, 0),  # N
-                 (-1, -1),  # NW
-                 (0, -1),  # W
-                 (1, -1),  # SW
-                 (1, 0),  # S
-                 (1, 1),  # SE
-                 ]
-    for dot in free_pos:
-        r, c = free_pos[0][0], free_pos[0][1]
-        table[r][c] = "B" if black_turn else "W"
-        change_neighbour_recursive(table, neighbors, len(table), len(table[0]), r, c, black_turn)
-        play(start_table, endings, free_pos[1:], table, not black_turn)
 
 
 if __name__ == "__main__":
     R = dumbothello("boards/01.txt")
     print(R)
+    # res = get_all_combinations_generator([1, 2, 3])
+    # for r in res:
+    #     print(r)
