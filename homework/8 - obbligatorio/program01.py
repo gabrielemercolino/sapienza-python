17eff6e1ef7d509eb61a65ff4aa6ec4ba1f529a4
gabrielemercolino <gmercolino2003@gmail.com>
2022-12-15 17:21:34 +0100

    finalmente funge

--- homework/8 - obbligatorio/program01.py
+++ homework/8 - obbligatorio/program01.py
@@ -66,6 +66,70 @@ def get_all_combinations_generator(lis):
                 yield [lis[i], *c]
 
 
+table = list[list[str]]
+endings = list[int, int, int]
+# neighbors = set[tuple[int, int]]
+neighbors = {
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1)
+}
+
+
+def calc_ending(state: table, endings: endings):
+    # print(*state, sep="\n", end="\n\n")
+    numW = len([cell for row in state for cell in row if cell == "W"])
+    numB = len([cell for row in state for cell in row if cell == "B"])
+    endings[:] = [endings[0] + int(numB > numW),
+                  endings[1] + int(numW > numB),
+                  endings[2] + int(numB == numW)]
+
+
+def check_free_pos(state: table):
+    return any(c for row in state for c in row if c == ".")
+
+
+def check_enemy_neighbors(state: table, pos: tuple[int, int], black_turn: bool):
+    enemy = "W" if black_turn else "B"
+    for neighbor in neighbors:
+        dr, dc = pos[0] + neighbor[0], pos[1] + neighbor[1]
+        if (0 <= dr < len(state)) and (0 <= dc < len(state[0])) and state[dr][dc] == enemy:
+            return True
+    return False
+
+
+def get_free_pos(state: table, black_turn: bool) -> list[tuple[int, int]]:
+    free_pos = [(r, c) for r, row in enumerate(state) for c, cell in enumerate(row) if cell == "."]
+    return [pos for pos in free_pos if check_enemy_neighbors(state, pos, black_turn)]
+
+
+def capture_enemy_neighbors(state: table, pos: tuple[int, int], black_turn: bool):
+    for neighbor in neighbors:
+        dr, dc = pos[0] + neighbor[0], pos[1] + neighbor[1]
+        if 0 <= dr < len(state) and 0 <= dc < len(state[0]) and state[dr][dc] != ".":
+            state[dr][dc] = "B" if black_turn else "W"
+
+
+def play(state: table, endings: endings, black_turn: bool):
+    if not check_free_pos(state):
+        calc_ending(state, endings)
+        return
+    all_free_cells = get_free_pos(state, black_turn)
+    if not all_free_cells:
+        calc_ending(state, endings)
+        return
+    for (r, c) in all_free_cells:
+        new_state = [list(row) for row in state]
+        new_state[r][c] = "B" if black_turn else "W"
+        capture_enemy_neighbors(new_state, (r, c), black_turn)
+        play(new_state, endings, not black_turn)
+
+
 def dumbothello(filename: str) -> tuple[int, int, int]:
     table = []
 
@@ -74,12 +138,11 @@ def dumbothello(filename: str) -> tuple[int, int, int]:
 
     endings = [0, 0, 0]
 
+    play(table, endings, black_turn=True)
+
     return tuple(endings)
 
 
 if __name__ == "__main__":
     R = dumbothello("boards/01.txt")
     print(R)
-    # res = get_all_combinations_generator([1, 2, 3])
-    # for r in res:
-    #     print(r)
